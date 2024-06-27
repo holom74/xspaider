ef3a8b5c21a4ec98eaf38756b10216fb1a353cc7
holom74 <93487984+holom74@users.noreply.github.com>
2024-06-27 17:54:01 +0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,25 @@
+// $('.container-fluid').animate({width:'100%'},1000)
+// $('.container-fluid').animate({height:'100vh'},1000 ,function(){
+//     $('.container-fluid h1').show(1000 ,function(){
+//         $('.container-fluid p ').fadeIn(1000,function(){
+//             $('.col-md-4').slideDown(1000)
+//         })
+//     })
+// })
+
+// console.log($('#about').offset().top);
+// let demoOffset = $('#demo').offset().top
+// // console.log(demoOffset);
+// // console.log($(window).scroll());
+// $(window).scroll(function(){
+//     // console.log($(window).scrollTop());
+//     let windowScroll =$(window).scrollTop()
+//     if(windowScroll>demoOffset -70){
+//         $('#header').css('backgroundColor', 'black')
+//         $('#toUp').css('display', 'block')
+//     }
+//     else {
+//         $('#header').css('backgroundColor', 'transparent')
+//         $('#toUp').css('display', 'none')
+//     }
+// })
